2a5d994d4b7859ffe9659ef9184d12e173608ff0
Ahad <ahadalisaeed@gmail.com>
2026-08-12 01:59:13 +0500

    changed Database URL

--- database/Database.py
+++ database/Database.py
@@ -12,7 +12,7 @@ DB_NAME = os.getenv("DB_NAME", "cybercafe_db")
 DB_USER = os.getenv("DB_USER", "cybercafe_admin")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "changeme123")
 
-DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql+psycopg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
